d90e8c5a1524f18130613b952b43f72dc73cafe4
Ilnytskyi-Oleh <44439724+Ilnytskyi-Oleh@users.noreply.github.com>
2021-07-06 16:10:24 +0300

    Update task2.js

--- js/task2.js
+++ js/task2.js
@@ -8,7 +8,7 @@ function getSymbol(){
 
     let userInput, number;
     do {
-        userInput = prompt('Введите символ на клавиатуре:');
+        userInput = prompt('Введите цифру на клавиатуре (от 0 до 9): ');
         number = parseInt(userInput);
         if(number > 9 || number < 0){
             number = null;
